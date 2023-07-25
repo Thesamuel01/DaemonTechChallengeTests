6fca2e5545e0e0b1472f51f302fe07ebe7ed12f4
samuel <samueldev8@gmail.com>
2023-07-25 02:19:05 -0300

    implement ConvertToDataTable pipe test

---
+++ ConvertToDataTablePipeTests.cs
@@ -0,0 +1,49 @@
+﻿using DaemonTechChallenge;
+using DaemonTechChallenge.ETL.Pipes;
+using DaemonTechChallenge.Helpers;
+using System.Data;
+using System.Threading.Tasks.Dataflow;
+
+namespace DaemonTechChallengeTests;
+
+public class ConvertToDataTablePipeTests
+{
+    private ConvertToDataTablePipe _pipe;
+    private IDataTableFormat _dataTableFormat;
+
+    public ConvertToDataTablePipeTests()
+    {
+        _dataTableFormat = new DataTableFormatHelper(new string[] { "Id", "Name" });
+        _pipe = new ConvertToDataTablePipe(_dataTableFormat);
+    }
+
+    [Fact]
+    public void TestCreateConvertToDataTableBlock()
+    {
+        var data = new List<TestData>
+            {
+                new TestData { Id = 1, Name = "Test 1" },
+                new TestData { Id = 2, Name = "Test 2" }
+            };
+
+        var block = _pipe.CreateConvertToDataTableBlock<TestData>();
+        block.Post(data);
+        block.Complete();
+
+        DataTable? result;
+        block.OutputAvailableAsync().Wait();
+        block.TryReceive(out result);
+
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Rows.Count);
+        Assert.Equal("Test 1", result.Rows[0]["Name"]);
+        Assert.Equal("Test 2", result.Rows[1]["Name"]);
+    }
+
+    private class TestData
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+}
---
+++ FetchZipDataPipeTests.cs
@@ -0,0 +1,62 @@
+﻿using DaemonTechChallenge.ETL.Pipes;
+using DaemonTechChallenge.Helpers;
+using Moq;
+using Moq.Protected;
+using System.Net;
+using System.Threading.Tasks.Dataflow;
+
+namespace DaemonTechChallengeTests;
+
+public class FetchZipDataPipeTests
+{
+    [Fact]
+    public void CreateFetchZipDataBlock_ReturnsTransformManyBlock()
+    {
+        // Arrange
+        var mockZiperHelper = new Mock<IZiperHelper>();
+        var mockHttpClient = new HttpClient();
+        var fetchZipDataPipe = new FetchZipDataPipe(mockZiperHelper.Object, mockHttpClient);
+
+        // Act
+        var result = fetchZipDataPipe.CreateFetchZipDataBlock(".txt");
+
+        // Assert
+        Assert.IsType<TransformManyBlock<string, MemoryStream>>(result);
+    }
+
+    [Fact]
+    public async Task CreateFetchZipDataBlock_DownloadsAndReadsData()
+    {
+        // Arrange
+        var handlerMock = new Mock<HttpMessageHandler>();
+        handlerMock
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            )
+            .ReturnsAsync(new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("[{'id':1,'value':'1'}]"),
+            })
+            .Verifiable();
+
+        var httpClient = new HttpClient(handlerMock.Object);
+        var mockZiperHelper = new Mock<IZiperHelper>();
+        mockZiperHelper.Setup(z => z.Read(It.IsAny<Stream>(), It.IsAny<string>()))
+            .Returns(new MemoryStream[] { new MemoryStream(new byte[] { 1, 2, 3 }) }.ToList());
+
+        var fetchZipDataPipe = new FetchZipDataPipe(mockZiperHelper.Object, httpClient);
+        var block = fetchZipDataPipe.CreateFetchZipDataBlock(".txt");
+
+        // Act
+        block.Post("https://example.com/data.zip");
+        block.Complete();
+        var result = await block.ReceiveAsync();
+
+        // Assert
+        Assert.Equal(new byte[] { 1, 2, 3 }, result.ToArray());
+    }
+}
