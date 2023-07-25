46a7690e858d38583a8dab429b31e0c9afa6ef12
samuel <samueldev8@gmail.com>
2023-07-25 02:57:23 -0300

    implement pipeline test

---
+++ PipelineTests.cs
@@ -0,0 +1,51 @@
+﻿using DaemonTechChallenge.ETL;
+using System.Threading.Tasks.Dataflow;
+
+namespace DaemonTechChallengeTests;
+
+public class PipelineTests
+{
+    [Fact]
+    public async Task Startup_PassesDataThroughPipeline()
+    {
+        // Arrange
+        var bufferBlock = new BufferBlock<int>();
+        var pipeline = new Pipeline(bufferBlock, null);
+
+        var transformBlock = new TransformBlock<int, int>(x => x * 2);
+        pipeline.AddStageToPipeline(transformBlock);
+
+        var actionBlock = new ActionBlock<int>(x => Assert.Equal(4, x));
+        pipeline.AddStageToPipeline(actionBlock);
+
+        // Act
+        _ = pipeline.Startup(2);
+        actionBlock.Complete();
+        await actionBlock.Completion;
+
+        // Assert
+        Assert.True(actionBlock.Completion.IsCompleted);
+    }
+
+
+    [Fact]
+    public void AddStageToPipeline_LinksBlocks()
+    {
+        // Arrange
+        var bufferBlock = new BufferBlock<int>();
+        var pipeline = new Pipeline(bufferBlock, null);
+
+        var transformBlock = new TransformBlock<int, int>(x => x * 2);
+        pipeline.AddStageToPipeline(transformBlock);
+
+        _ = pipeline.Startup(2);
+
+        // Act
+        int result;
+        transformBlock.OutputAvailableAsync().Wait();
+        transformBlock.TryReceive(out result);
+
+        // Assert
+        Assert.Equal(4, result);
+    }
+}
